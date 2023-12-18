d0ca8408dc8d8040f3f9975651708e48bfaf3fb2
Moises Menezes <moisesmenezes53@gmail.com>
2023-12-17 23:41:22 -0300

    Create SideBar Component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function SideBar() {
+  const profileImage =
+    "https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src={profileImage} />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/mgmenezes.png" />
+        <strong>Moisés</strong>
+        <span>Sofware Engineer</span>
+      </div>
+
+      <footer className={styles.footer}>
+        <a href="http://">
+          <PencilLine size={20} />
+          Editar seu Perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
